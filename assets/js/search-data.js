efebacd92bd395b63ab540e791936ce57ffea800
eaglema <65811915+eaglema@users.noreply.github.com>
2026-01-08 02:49:07 +0000

    Deploying to gh-pages from @ eaglema/eaglema.github.io@2240e706be725688cd7fb24983f7a8273e8ca4f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "#Contribute equally.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
